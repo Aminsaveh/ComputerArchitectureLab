ffb1b7fd632d65830f3ef7d24d98e220722dfb5c
Kamyab <kamyababedi@gmail.com>
2021-04-10 14:36:53 +0200

    Update 002

--- 002 ALU 74181/Code/Project/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/pixel/Desktop/az3/testbench_isim_beh.exe" -prj "C:/Users/pixel/Desktop/az3/testbench_beh.prj" "work.testbench" "work.glbl" 
